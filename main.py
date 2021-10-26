c5ff82db69c3b9bcc10a8cdc226774eab5f4f5bf
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-10-26 22:59:25 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,70 @@
+
+
+
+*****targil 3:
+
+ls = [1,2,3,4,5,6]
+for i in range(0,len(ls)//2):
+    print(ls[i])
+
+
+
+
+
+*****targil 4:
+
+
+ls1 = ['hello', 'python', 'pen', 'world of coding']
+for i in range(0, len(ls1)):
+    first = str(ls1[i])
+    print(first.upper())
+    
+    
+*****targil 5:
+
+ls1 = ['hello', 'python', 'pen', 'world of coding']
+for i in range(0, len(ls1)):
+    first = str(ls1[i])
+    if len(first) < 4 :
+        break
+    print(first.upper())
+    
+
+*****targil 6:
+
+fullname = 'Julie Sigal'
+print(fullname[-5:len(fullname)])
+print(fullname[0:(len(fullname)//3)])
+print(fullname.count('a'))
+#
+ls_fullname = fullname.split()
+ls_fullname = [ls_fullname[1] , ls_fullname[0]]
+print(ls_fullname)
+ls_fullname[0] = ls_fullname[0].upper()
+ls_fullname[1] = ls_fullname[1].replace('l',"")
+fullname_2 = ' '.join(ls_fullname)
+print(fullname_2)
+
+*****targil 7:
+
+
+
+
+
+
+***** targil 9:
+
+
+# new_list = [8,1000,-3,2,5]
+# print(sum(new_list))
+# print((max(new_list)))
+# print(min(new_list))
+# print(sum(new_list) / len(new_list))
+# print(new_list.remove(-3))
+# new_list.sort()
+# print(new_list)
+# print(new_list*5)
+# new2 = new_list[1:len(new_list)]
+
+
+
